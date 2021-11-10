5031c236979cdb9ec763735b9abac541c6accc51
Konstantin Sekret <sekret.constantine@gmail.com>
2021-11-10 07:53:24 +0300

    some adjustments

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import React from 'react'
 import './styles.css'
 import DraggableList from './App'
 
-render(<DraggableList items={'Lorem ipsum dolor sit'.split(' ')} />, document.getElementById('root'))
+const items = [
+  { name: 'compact', commented: false },
+  { name: 'takeWhile', commented: false },
+  { name: 'filter', commented: true },
+  { name: 'skip(1)', commented: false },
+  { name: 'shareReply', commented: true }
+]
+
+render(<DraggableList items={items} />, document.getElementById('root'))
