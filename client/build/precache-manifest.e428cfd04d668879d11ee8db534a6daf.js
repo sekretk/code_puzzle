9d5a542d1823295459862631284d6fdc01020d32
Konstantin Sekret <sekret.constantine@gmail.com>
2021-11-11 10:24:24 +0300

    build

---
+++ client/build/precache-manifest.e428cfd04d668879d11ee8db534a6daf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c03c6fb7d065ff2b751bdd7ed08637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d12d742eaa42c6e2b4",
+    "url": "/static/css/main.557b5dad.chunk.css"
+  },
+  {
+    "revision": "1c6e156b4dfd622fbc4c",
+    "url": "/static/js/2.8b9bf873.chunk.js"
+  },
+  {
+    "revision": "d1d12d742eaa42c6e2b4",
+    "url": "/static/js/main.c8a5d401.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
