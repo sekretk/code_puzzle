836097e6c0b0c14aedc7f84df6c8da34ff5198d0
sekretk <boyko.constantine@gmail.com>
2021-11-13 12:02:25 +0300

    result text

--- client/src/Result.js
+++ client/src/Result.js
@@ -13,7 +13,9 @@ export default function Result({ links, text }) {
                     ))
                 }
             </ul>
-            <p>Поучаствуй в розыгрыше ватрушки для зимних катаний! Оставь свои контакты <a href="https://docs.google.com/forms/d/1_qOo9r05XTCCKvgIGJChXD3FlPAzOBLYdkxjxsJb55U/edit" target="_blank">Тут</a> и жди результатов сегодня в 19:00</p>
+            <p>Спасибо за участие!</p>
+            <p>Подойди к стенду, назови "Fintech Forever" и получи сувенир от команды Devexperts</p>
+            <p>А чтобы принять участие в розыгрыше супер-приза, надо заполнить <a href="https://docs.google.com/forms/d/1_qOo9r05XTCCKvgIGJChXD3FlPAzOBLYdkxjxsJb55U/edit" target="_blank">анкету</a>. <br/> Ватрушки для зимних катаний с горки ждёт победителя! :) Розыгрыш сегодня в 19:00</p>
         </>
     );
 }
