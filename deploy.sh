271b45eb017a8e386d8893f1ca1fe6e0699f391a
sekretk <boyko.constantine@gmail.com>
2022-05-11 10:15:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git reset --hard && git pull && cd api && npm run build && cd .. && pm2 restart code_puzzle_api && cp -r ./client/build/* /var/www/quiz.boysthings.top/html/
+git reset --hard && git pull && cd api && npm run build && cd .. && pm2 restart quiz && cp -r ./client/build/* /var/www/quiz.boysthings.top/html/
