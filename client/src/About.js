7a2e723ad291c8fba67177c68446c2473889b601
Ygeorge <g.ymydykov@gmail.com>
2022-05-16 14:26:00 +0300

    Translate About.js

--- client/src/About.js
+++ client/src/About.js
@@ -15,40 +15,37 @@ export default function Result({ links, text }) {
                     width="150" height="30" />
             </a>
 
-            <p>Международная карьера в Fintech:</p>
-            <p><a href="https://devexperts.com/about-devexperts/" target="_blank">Подробнее о наших продуктах и команде</a></p>
-            <p><a href="https://careers.devexperts.com/vacancies/?country=ru&remote=true" target="_blank">Наши вакансии</a></p>
+            <p>20 years of experience building capital markets software solutions:</p>
+            <p><a href="https://devexperts.com/about-devexperts/" target="_blank">Learn more about our team and products</a></p>
+            <p><a href="https://careers.devexperts.com/vacancies/?country=ru&remote=true" target="_blank">View open positions</a></p>
 
             <br />
-            <h3>Правила игры:</h3>
+            <h3>Game rules:</h3>
             <p className="rules">
-                Приложение предлагает решить задачу.<br />
-                Для пропуска нужно нажать "Далее".<br />
-                Для отправки на проверку - "Отправить".<br />
-                Для комментирования строки нужно нажать на "//".<br />
-                На основе фидбэка от 5 минутного тестирования нужно добавить:
-                <ul>
-                    <li>- Вы ВСЕГДА получаете случайное задание - так что жмите "Далее" если уже решали.</li>
-                    <li>- При старте закомментированы случайные элементы - не позволяйте этому выбору повлиять на вас!</li>
-                </ul>
-                Допустимы следующие флаги на задачах:
+                We suggest you to resolve a bucnh of tasks.<br />
+                To skip the task tap "Continue".<br />
+                To submit your solution tap - "Submit".<br />
+                For commentning the line tap on "//".<br />
+                You always get randomized task, tap "Continue" if you already resolved it before.<br />
+                JFYI: we comment random elements when generate tasks for you.<br />
+                The following flags are allowed:
             </p>
 
             <span className="badge sort">
                 <span className="badge_icon material-icons material-icons-outlined">
                     sort
                 </span>
-                Необходимо отсортировать
+                Sorting is required
             </span>
 
             <span className="badge multiple">
                 <span className="badge_icon material-icons material-icons-outlined">
                     checklist_rtl
                 </span>
-                Допустимо несколько вариантов
+                Multiple choices allowed
             </span>
 
-            <button className="go" onClick={onGo}>Начать!</button>
+            <button className="go" onClick={onGo}>Start!</button>
         </>
     );
 }
