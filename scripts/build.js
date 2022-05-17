6206f78f3acdc6e10cc2faed1646d99c062fbd1e
Konstantin Sekret <sekret.constantine@gmail.com>
2022-05-17 14:23:21 +0300

    notify

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,47 @@
 const { execSync } = require("child_process");
 const { chdir } = require("process");
 const path = require('path');
+require('dotenv').config();
+const https = require('https')
+const querystring = require("querystring");
+
+const sendNotification = (message) => {
+
+    const options = {
+        hostname: 'api.telegram.org',
+        port: 443,
+        path: encodeURI()`/${process.env.BOT_TOCKEN}/sendMessage?chat_id=${process.env.NOTIFY_CHAT}&text=${message}`),
+        method: 'GET'
+      }
+      
+      const req = https.request(options, res => {
+          console.log(`statusCode: ${res.statusCode}`)
+        
+          res.on('data', d => {
+            process.stdout.write(d)
+          })
+        })
+        
+        req.on('error', error => {
+          console.error(error)
+        })
+        
+        req.end()
+}
+
 
 const run = (command) => {
     console.log('RUN', command);
     const result = execSync(command);
     console.log('OUTPUT', result.toString());
 }
+
 run('git reset --hard');
-run('git pull');
+
+if (execSync('git pull').toString().includes('Already up to date')) {
+    return;
+}
+
 chdir(path.join(__dirname, '../api'));
 run('npm i');
 run('npm run build');
@@ -17,3 +50,4 @@ run('npm i');
 run('npm run build');
 chdir(path.join(__dirname, '..'));
 run('cp -r ./client/build/* /var/www/quiz.boysthings.top/html/');
+sendNotification('Updated')
