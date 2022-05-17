0751a21426d99fc57c87f87b966bd27efc9aec5f
Konstantin Sekret <sekret.constantine@gmail.com>
2022-05-17 12:23:14 +0300

    build

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 git reset --hard
 git pull
 cd api
+npm i
 npm run build
 cd .. && pm2 restart quiz
 cd client
+npm i
 npm run build
 cd ..
 cp -r ./client/build/* /var/www/quiz.boysthings.top/html/
