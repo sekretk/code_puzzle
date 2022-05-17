b49ab1a0b2f1b4cb71779256bd132069dd221783
Konstantin Sekret <sekret.constantine@gmail.com>
2022-05-17 12:56:46 +0300

    build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 const { execSync } = require("child_process");
+const { chdir } = require("process");
 
 const run = (command) => {
     console.log('RUN', command);
@@ -7,9 +8,10 @@ const run = (command) => {
 }
 run('git reset --hard');
 run('git pull');
-run('cd api');
+chdir('api');
 run('npm i');
 run('npm run build');
+chdir('cd ../client');
+run('npm i');
 run('npm run build');
-run('cd ..');
 run('cp -r ./client/build/* /var/www/quiz.boysthings.top/html/');
